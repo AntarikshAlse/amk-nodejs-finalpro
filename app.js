9072418d07c89f943ee8a0dc4d6dd53c7d7f9308
Int-coder <74238119+Int-coder@users.noreply.github.com>
2021-06-23 12:48:56 +0530

    test4

--- app.js
+++ app.js
@@ -23,8 +23,10 @@ app.use(bodyParser.json());
 app.use((req,res,next)=>{
     res.header("Access-Control-Allow-Origin","*");
     res.header("Access-Control-Allow-Headers",'Origin,X-Requested-With,Content-Type,application/json,Authorization,Accept');
-        res.header("Access-Control-Allow-Methods","PUT,POST,PATCH,OPTIONS,DELETE,GET");
-        // return res.status(200).json();
+    if(req.method==="OPTION"){
+        res.header("Access-Control-Allow-Methods","*");
+        return res.status(200).json();
+    }
     next();
 });
 
