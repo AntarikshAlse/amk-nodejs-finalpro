e791daea9511e308c8087afaa051e0729075b839
Int-coder <74238119+Int-coder@users.noreply.github.com>
2021-06-23 12:00:04 +0530

    check :typos resolve

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((req,res,next)=>{
     res.header("Access-Control-Allow-Origin","*");
     res.header("Access-Control-Allow-Header",'origin,X-Requested-Width,Content-Type,Authorization');
     if(req.method==="OPTION"){
-        res.header("Access-Control-Allow-Methode","PUT,POST,PATCH,DELETE,GET");
+        res.header("Access-Control-Allow-Methods","PUT,POST,PATCH,DELETE,GET");
         return res.status(200).json();
     }
     next();
